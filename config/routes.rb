442c2c70dab95267026096ee7a271710ca5e1c87
Daniel Perini <dsperini@gmail.com>
2021-10-20 16:37:03 -0300

    view task implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #index
   get 'tasks', to: 'tasks#index'
+
+  #show
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
